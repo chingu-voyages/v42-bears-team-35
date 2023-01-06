1bc90fc293912fe1e8a51714dac81f8f8c140fd4
Andrés Court <andres@andrescourt.com>
2023-01-05 21:31:10 -0500

    added swagger documentation capabilities

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import swaggerUi from "swagger-ui-express";
 import AppDataSource from "./db";
 import { Test } from "./model";
 import { PORT } from "../environment";
+import swagger from "./swagger/swagger.json";
 
 const app: Express = express();
 const port = PORT;
@@ -12,6 +14,8 @@ app.get("/", async (req: Request, res: Response) => {
   const data = await testRepository.find();
   res.json(data);
 });
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swagger));
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console

611d1bf635adaab0b4a1d32ce18518b1e6c6ec87
Andrés Court <andres@andrescourt.com>
2023-01-09 21:08:47 -0500

    jest setup

---
+++ server/jest.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  testMatch: ["**/__tests__/*.ts"],
+};
+
+export default config;
---
+++ server/src/__tests__/setup.test.ts
@@ -0,0 +1,5 @@
+describe("setup", () => {
+  it("should expect true", () => {
+    expect(true).toBe(true);
+  });
+});

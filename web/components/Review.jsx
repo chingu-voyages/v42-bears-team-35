e09d653b20ea1896f6ab70a1a3016c01e593bba9
Tijana Jung <tloochich@icloud.com>
2023-01-12 18:56:22 -0500

    styling reviews in progress.

--- web/components/Review.jsx
+++ web/components/Review.jsx
@@ -1,25 +1,45 @@
-import { StyleSheet, Text } from "react-native"
+import { ScrollView, StyleSheet, Text, View } from "react-native"
 
 const Review = ({name, review, rating, date}) => {
 
+    const stars = <>
+    
+    </>
     const paragraphs = review.split('/n')
     const style = StyleSheet.create({
+        container: {
+            padding: 24
+        },
         name: {
             backgroundColor: "#d9d9d9",
-            padding: 8,
-            fontSize: 20
+            padding: 16,
+            paddingTop: 8,
+            paddingBottom: 8,
+            fontSize: 20,
+            marginRight: 8,
+            borderRadius: 6
         },
         review: {
             fontSize: 20,
-            marginBottom: 12
+            marginBottom: 12,
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center"
+        },
+        row: {
+            display: "flex",
+            flexDirection: "row"
         }
     })
 
     return (
-        <>
-            <Text style={style.name}>{name}</Text>
+        <ScrollView style={style.container}>
+            <View style={style.row}>
+                <Text style={style.name}>{name}</Text>
+                <Text>{Intl.DateTimeFormat('us-EN').format(date)}</Text>
+            </View>
             {paragraphs.map(p => <Text style={style.review}>{p}</Text>)}
-        </>
+        </ScrollView>
     )
 }
 

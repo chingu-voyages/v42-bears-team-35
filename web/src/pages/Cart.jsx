2c1385bbc8eae493fa9d3b5bd669d832ff263488
Tijana Jung <tloochich@icloud.com>
2023-01-25 10:26:08 -0500

    set bg color

--- web/src/pages/Cart.jsx
+++ web/src/pages/Cart.jsx
@@ -1,5 +1,19 @@
+import { StyleSheet } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
 
+const style = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#222020"
+    }
+})
 
 export default Cart => () => {
     // user can see all items in cart
+    return (
+        <ScrollView style={style.container}>
+
+        </ScrollView>
+    )
 }

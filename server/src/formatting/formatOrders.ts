f053638b1a70e60de32d98a9844f79b5438594e1
Andrés Court <andres@andrescourt.com>
2023-02-09 19:50:00 -0500

    Feature: Format the order response

--- server/src/formatting/formatOrders.ts
+++ server/src/formatting/formatOrders.ts
@@ -1,28 +1,38 @@
-import { Order } from "../model";
+import { FormattedItemResponse } from "../types/ItemTypes";
+import { Order, OrderItem } from "../model";
+import { formatOneItem } from "./formatItems";
 
 export interface OrderResponseInterface {
   id: string;
   total: number;
+  status: string;
+  date: Date;
+  orderItem: OrderItemResponseInterface[];
 }
 
 export interface OrderItemResponseInterface {
   quantity: number;
   cost: number;
   total: number;
+  product: FormattedItemResponse;
 }
 
 export function formatOneOrder(order: Order): OrderResponseInterface {
-  // const orderItemsResponse = order.orderItems.map((orderItem: OrderItem) => {
-  //   return {
-  //     quantity: orderItem.quantity,
-  //     cost: orderItem.cost,
-  //     total: orderItem.total,
-  //   };
-  // });
+  const orderItemsResponse = order.orderItems.map((orderItem: OrderItem) => {
+    return {
+      quantity: orderItem.quantity,
+      cost: orderItem.cost,
+      total: orderItem.total,
+      product: formatOneItem(orderItem.item),
+    };
+  });
 
   return {
     id: order.id,
     total: order.total,
+    status: order.status,
+    date: order.created_at,
+    orderItem: orderItemsResponse,
   };
 }
 
